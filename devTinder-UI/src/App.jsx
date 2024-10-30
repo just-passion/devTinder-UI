bea6c74a472397f53fc5c529d707928e6ff36039
just-passion <tinycolorfulhummingbird@gmail.com>
2024-10-30 21:38:02 +0530

    browser router component added

--- devTinder-UI/src/App.jsx
+++ devTinder-UI/src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>Login Page</div>}></Route>
+          <Route path="/login" element={<div>Login Page</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <NavBar />
     </>
   );
 }
